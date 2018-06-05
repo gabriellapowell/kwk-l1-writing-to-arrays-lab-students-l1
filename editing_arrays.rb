6d30835f101754cb06d318c7c20f53d87262bc3c
gabriellapowell <soccerplayer60@icloud.com>
2018-06-05 22:31:28 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -6,8 +6,8 @@ def change_rainbow_colors
   rainbow_colors = [ "yellow",
   "default",
   "light_cyan"]
-rainbow_colors[0] = "red"
-rainbow_colors[1] = "light_red"
+change_rainbow_colorsrainbow_colors[0] = "red"
+change_rainbow_colorsrainbow_colorsrainbow_colors[1] = "light_red"
 rainbow_colors[2] = "light_yellow"
 rainbow_colors[3] = "green"
 rainbow_colors[4] = "blue"
